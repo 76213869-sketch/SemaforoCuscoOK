cb89532a6eeaa3d702a861d5731dd341d586ba9c
Antony <76213869@continental.edu.pe>
2026-06-17 14:00:24 -0500

    debug env full

--- env_debug.php
+++ env_debug.php
@@ -2,5 +2,15 @@
 
 header('Content-Type: text/plain');
 
-echo "PRUEBA=";
+echo "=== _ENV ===\n";
+print_r($_ENV);
+
+echo "\n=== _SERVER ===\n";
+print_r(array_filter($_SERVER, function($k){
+    return strpos($k,'MYSQL') !== false
+        || strpos($k,'RAILWAY') !== false
+        || strpos($k,'PRUEBA') !== false;
+}, ARRAY_FILTER_USE_KEY));
+
+echo "\n=== getenv(PRUEBA) ===\n";
 var_dump(getenv('PRUEBA'));
